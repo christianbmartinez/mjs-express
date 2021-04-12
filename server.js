b25a76d2350034e68695ca0a5806745d26e0adc3
Christian Martinez <christian@mixdlabs.com>
2021-04-12 07:39:08 -0600

    First commit!

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+require('dotenv').config()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('index.html') 
+})
+
+app.get('/about.html', (req, res) => {
+    res.render('about.html')
+})
+
+app.listen(process.env.PORT, () => {console.log(`Server listening on port ${process.env.PORT}`)})
